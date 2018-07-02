b373970144b143939de017d6cbc26466fbf8e10f
Scoti Dodson <scotidodson@gmail.com>
2018-07-02 18:10:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -32,3 +32,7 @@ var chocolateBars = [
 function addElementToEndOfArray(array, element) {
   return `${array}.push("${element}")`;
 }
+
+function addElementToEndOfArray(array, element) {
+  return `${array}.push("${element}")`;
+}
