691a512a3933e69790c6cd2f2a4a66e0b8a011b1
Scoti Dodson <scotidodson@gmail.com>
2018-07-02 18:29:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -36,7 +36,7 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  `${array}.unshift("${element}")`;
+  ${array}.unshift(${element});
 }
 
 function addElementToEndOfArray(array, element) {
